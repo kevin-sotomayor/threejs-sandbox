6f0dd18e23859793d2ff33aff6614187be42f796
La-li-lu-le-lo <kevin.sotomayor@outlook.fr>
2025-04-02 10:21:26 +0200

    starting point is now correct

--- src/main.js
+++ src/main.js
@@ -9,6 +9,10 @@ const app = document.querySelector(".app");
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(50, app.clientWidth / app.clientHeight, 0.1, 1000);
 
+camera.position.set(0, 0, 1);
+camera.lookAt(0, 0, 0);
+camera.aspect = app.clientWidth / app.clientHeight;
+
 const axesHelper = new THREE.AxesHelper(100);
 scene.add(axesHelper);
 
@@ -33,12 +37,20 @@ controls.autoRotate = false;
 
 
 const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0x00aaff });
+const material = new THREE.MeshBasicMaterial({ color: 0x00aaff, wireframe: true });
 const cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
+
+window.addEventListener("resize", () => {
+    camera.aspect = app.clientWidth / app.clientHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(app.clientWidth, app.clientHeight);
+    renderer.setPixelRatio(window.devicePixelRatio);
+});
 
 function renderLoop() {
 	controls.update();
 	renderer.render(scene, camera);
 	window.requestAnimationFrame(renderLoop);
+	camera.updateProjectionMatrix();
 }
